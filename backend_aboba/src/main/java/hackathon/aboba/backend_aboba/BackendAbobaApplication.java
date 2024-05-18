b57a64b310953d047be6d1f161c71300df99023b
trubinovta <t.trubinov@g.nsu.ru>
2024-05-18 13:04:55 +0700

    spring project

---
+++ backend_aboba/src/main/java/hackathon/aboba/backend_aboba/BackendAbobaApplication.java
@@ -0,0 +1,13 @@
+package hackathon.aboba.backend_aboba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAbobaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAbobaApplication.class, args);
+	}
+
+}
---
+++ backend_aboba/src/test/java/hackathon/aboba/backend_aboba/BackendAbobaApplicationTests.java
@@ -0,0 +1,13 @@
+package hackathon.aboba.backend_aboba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAbobaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
