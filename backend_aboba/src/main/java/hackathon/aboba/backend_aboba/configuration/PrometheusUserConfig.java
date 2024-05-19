79fdb2720437ed4321437c8edc8ac06cbe7d056d
trubinovta <t.trubinov@g.nsu.ru>
2024-05-19 13:21:05 +0700

    Integration tests

--- backend_aboba/src/main/java/hackathon/aboba/backend_aboba/configuration/PrometheusUserConfig.java
+++ backend_aboba/src/main/java/hackathon/aboba/backend_aboba/configuration/PrometheusUserConfig.java
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 
+@Profile("!testing")
 @Configuration
 @Component
 @RequiredArgsConstructor
---
+++ backend_aboba/src/test/java/hackathon/aboba/backend_aboba/integration/CategoriesTest.java
@@ -0,0 +1,29 @@
+package hackathon.aboba.backend_aboba.integration;
+
+import java.util.List;
+
+import hackathon.aboba.backend_aboba.dto.CategoryDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@WithMockUser(username = "myUser")
+class CategoriesTest extends AbstractIntegrationTest {
+
+    @Test
+    void addCategoriesTest() throws Exception {
+        var body = new CategoryDto("category", 5L, "emoji");
+        mockMvc.perform(post("/api/v1/categories")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(body)))
+                .andDo(print());
+
+        mockMvc.perform(get("/api/v1/categories/all"))
+                .andExpect(content().string(objectMapper.writeValueAsString(List.of(body))));
+    }
+}
