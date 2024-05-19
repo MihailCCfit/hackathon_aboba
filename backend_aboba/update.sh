4ebac96ddf2cd5fd98d9c84eb83323d9f26ceb37
trubinovta <t.trubinov@g.nsu.ru>
2024-05-19 14:51:39 +0700

    deploy fix

--- backend_aboba/update.sh
+++ backend_aboba/update.sh
@@ -5,4 +5,4 @@ ls -al;
 sudo docker ps;
 sudo docker stop aboba_container || true;
 sudo docker load -i aboba_backend.tar;
-sudo docker run --rm -d -p 8090:8090 --name aboba_container aboba_backend:latest;
+sudo docker run --rm -d -p 8050:8050 --name aboba_container aboba_backend:latest;
